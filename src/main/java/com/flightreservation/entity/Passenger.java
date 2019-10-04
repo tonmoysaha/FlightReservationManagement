694b770a1cb026d8d77b52d186313f47374a489d
tonmoysaha <tonmoysahaopi@gmail.com>
2019-10-03 19:34:53 -0700

    adding passenger class

---
+++ src/main/java/com/flightreservation/entity/Passenger.java
@@ -0,0 +1,59 @@
+package com.flightreservation.entity;
+
+public class Passenger {
+	private int id;
+	private String fristName;
+	private String lastName;
+	private String middleName;
+	private String email;
+	private String phone;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFristName() {
+		return fristName;
+	}
+
+	public void setFristName(String fristName) {
+		this.fristName = fristName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
