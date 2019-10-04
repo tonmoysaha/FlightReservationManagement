c28fd1d930aa478058a5960ca5175a8644518fea
tonmoysaha <tonmoysahaopi@gmail.com>
2019-10-03 19:21:36 -0700

    adding user model class

---
+++ src/main/java/com/flightreservation/entity/User.java
@@ -0,0 +1,51 @@
+package com.flightreservation.entity;
+
+public class User {
+
+	private int id;
+	private String fristName;
+	private String lastName;
+	private String email;
+	private String Password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFristName() {
+		return fristName;
+	}
+
+	public void setFristName(String fristName) {
+		this.fristName = fristName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+}
