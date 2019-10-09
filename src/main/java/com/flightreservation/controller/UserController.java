f296ea137939e0ca776c3da6d0a27a991d039602
tonmoysaha <tonmoysahaopi@gmail.com>
2019-10-09 11:20:59 +0600

    adding reg controller and login page

--- src/main/java/com/flightreservation/controller/UserController.java
+++ src/main/java/com/flightreservation/controller/UserController.java
@@ -1,13 +1,31 @@
 package com.flightreservation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.flightreservation.entity.User;
+import com.flightreservation.repository.UserRepository;
 
 @Controller
 public class UserController {
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping("/regUser")
-	public String useRegs() {
-		return "regestrationPage";
+	public String useRegs(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		return "regPage";
+	}
+	
+	@RequestMapping(value = "/registerUser" , method = RequestMethod.POST)
+	public String regestrationUser(@ModelAttribute("user") User user) {
+		userRepository.save(user);
+		return "login";
+		
 	}
 }
