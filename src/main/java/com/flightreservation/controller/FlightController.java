c5e4edaba71a621a13200369a9eaf4affd6b44fe
tonmoysaha <tonmoysahaopi@gmail.com>
2019-10-17 23:18:29 +0600

    adding repository findflights

--- src/main/java/com/flightreservation/controller/FlightController.java
+++ src/main/java/com/flightreservation/controller/FlightController.java
@@ -1,26 +1,31 @@
 package com.flightreservation.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.flightreservation.entity.Flight;
 import com.flightreservation.repository.FlightRepository;
 
 @Controller
 public class FlightController {
-    @Autowired
+	@Autowired
 	private FlightRepository flightRepository;
-	
-	
+
 	@RequestMapping(value = "/findFlights", method = RequestMethod.POST)
-	public String findFlights(@RequestParam("form") String form,@RequestParam("to") String to,@RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") Date departureDate) {
-		flightRepository.findFlights(form,to,departureDate);
+	public String findFlights(@RequestParam("form") String form, @RequestParam("to") String to,
+			@RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") Date departureDate, ModelMap map) {
+		List<Flight> flights = flightRepository.findFlights(form, to, departureDate);
+
+		map.addAttribute("flights", flights);
 		return "displayFilghts";
-		
+
 	}
 }
--- src/main/java/com/flightreservation/repository/FlightRepository.java
+++ src/main/java/com/flightreservation/repository/FlightRepository.java
@@ -1,9 +1,18 @@
 package com.flightreservation.repository;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.flightreservation.entity.Flight;
 
 
 public interface FlightRepository extends CrudRepository<Flight, Long> {
+
+	@Query("from Flight where departureCity= :departureCity and arrivalCity= :arrivalCity and dateOfDeparture= :dateOfDeparture")
+	List<Flight> findFlights(@Param("departureCity")String form,@Param("arrivalCity") String to,@Param("dateOfDeparture") Date departureDate);
 
 }
