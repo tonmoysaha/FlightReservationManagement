4c4e15cae6fec2fe908e7f07b87dfa0d5817ca42
tonmoysaha <tonmoysahaopi@gmail.com>
2019-12-28 12:50:55 +0600

    adding checkin controller

---
+++ checkout/checkout/src/main/java/com/flightcheckout/controller/FlightCheckinController.java
@@ -0,0 +1,32 @@
+package com.flightcheckout.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.flightcheckout.entity.Reservation;
+import com.flightcheckout.integration.ReservationRestClient;
+
+@Controller
+public class FlightCheckinController {
+	
+	@Autowired
+	private ReservationRestClient reservationRestClient;
+	
+	@RequestMapping("/startCheckIn")
+	public String showStartCheckinPage() {
+		return "startCheckIn";
+
+	}
+	@RequestMapping(value = "/startCheckIn" , method = RequestMethod.POST)
+	public String startCheckIn(@RequestParam("reservationId") Long reservationId, ModelMap map) {
+		Reservation reservation = reservationRestClient.findReservation(reservationId);
+		map.addAttribute("reservation" , reservation);
+		return "dispalyReservationDetails";
+		
+	}
+
+}
