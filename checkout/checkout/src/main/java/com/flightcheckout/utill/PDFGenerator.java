d262dfdec51823f6e89f7c96593a9997a1f6a464
tonmoysaha <tonmoysahaopi@gmail.com>
2019-12-29 10:02:44 +0600

    adding flight and passenger details to pdfgenerator

--- checkout/checkout/src/main/java/com/flightcheckout/utill/PDFGenerator.java
+++ checkout/checkout/src/main/java/com/flightcheckout/utill/PDFGenerator.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Component;
 import com.flightcheckout.entity.Reservation;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
@@ -25,10 +28,60 @@ public class PDFGenerator {
 		PdfWriter.getInstance(document, new FileOutputStream(filePath));
 		
 		document.open();
-		document.add(new PdfPTable(2));
+		document.add(generateTable(reservation));
 		document.close();
 			
 		
+	}
+
+	private PdfPTable generateTable(Reservation reservation) {
+		// TODO Auto-generated method stub
+		PdfPTable table = new PdfPTable(2);
+		PdfPCell cell;
+		
+		cell = new PdfPCell(new Phrase("Flight Itinerary"));
+		cell.setColspan(2);
+		table.addCell(cell);
+		
+		cell = new PdfPCell(new Phrase("Flght Detials: "));
+		cell.setColspan(2);
+		table.addCell(cell);
+		
+		table.addCell("Operating Airlines");
+		table.addCell(reservation.getFlight().getOperatingAirlines());
+		
+		table.addCell("Departure City");
+		table.addCell(reservation.getFlight().getDepartureCity());
+		
+		table.addCell("Arrival City");
+		table.addCell(reservation.getFlight().getArrivalCity());
+		
+		table.addCell("Flight Number");
+		table.addCell(reservation.getFlight().getFlightNumber());
+		
+		table.addCell("Date Of Departure");
+		table.addCell(reservation.getFlight().getDateOfDeparture().toString());
+		
+		table.addCell("Estimated Departure Time");
+		table.addCell(reservation.getFlight().getEstimatedDepartureTime().toString());
+		
+		cell = new PdfPCell(new Phrase("Passenger Details: "));
+		cell.setColspan(2);
+		table.addCell(cell);
+		
+		table.addCell("First Name");
+		table.addCell(reservation.getPassenger().getFristName()); 
+		
+		table.addCell("Last Name");
+		table.addCell(reservation.getPassenger().getLastName());
+		
+		table.addCell("Email");
+		table.addCell(reservation.getPassenger().getEmail());
+		
+		table.addCell("Phone");
+		table.addCell(reservation.getPassenger().getPhone());
+		
+		return table;
 	}
 
 }
