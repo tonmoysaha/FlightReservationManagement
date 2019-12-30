003b067481c27f789f15bf2436e05d844933cab9
tonmoysaha <tonmoysahaopi@gmail.com>
2019-12-30 14:26:53 +0600

    adding logback.xml for gather loggerinformation

--- flightreservation/src/main/java/com/flightreservation/serviceImpl/ReservationServiceImpl.java
+++ flightreservation/src/main/java/com/flightreservation/serviceImpl/ReservationServiceImpl.java
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.flightreservation.controller.FlightController;
 import com.flightreservation.dto.ReservationRequest;
 import com.flightreservation.entity.Flight;
 import com.flightreservation.entity.Passenger;
